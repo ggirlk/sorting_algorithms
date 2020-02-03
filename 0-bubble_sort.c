1dd4e144c38c4329ddf049565c4865d9e3c514f8
ggirlk <ggirlk2@gmail.com>
2020-02-03 01:29:54 +0000

    test

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,28 +6,24 @@
  * bubble_sort - sorts an array of integers
  * ************* in ascending order using
  * ************* the Bubble sort algorithm
+ * @array: array of integer
+ * @size: size of array
  * Return: nothing
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j, s;
+size_t i, j;
 for (j = 0; j < size; j++)
 {
 for (i = 1; i < size; i++)
 {
-if (array[i-1] > array[i])
+if (array[i - 1] > array[i])
 {
-int k = array[i-1];
-array[i-1] = array[i];
+int k = array[i - 1];
+array[i - 1] = array[i];
 array[i] = k;
-s = 1;
 }
-else
-s = 0;
+}
 print_array(array, size);
-}
-if (s == 0)
-break;
-
 }
 }
