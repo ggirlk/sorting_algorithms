528033760d7931f14d7df8be72988d949abdafbc
khaldi505 <habetlbrika69@gmail.com>
2020-02-05 23:06:03 +0100

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,10 +10,8 @@
 void selection_sort(int *array, size_t size)
 {
 size_t i, j;
-int min_idx, temp;
-if (size < 2)
-return;
-if (array == NULL)
+unsigned int min_idx, temp;
+if (size == 0)
 return;
 for (i = 0; i < size - 1; i++)
 {
@@ -21,9 +19,12 @@ min_idx = i;
 for (j = i + 1; j < size; j++)
 if (array[j] < array[min_idx])
 min_idx = j;
+if(min_idx != i)
+{
 temp = array[min_idx];
 array[min_idx] = array[i];
 array[i] = temp;
 print_array(array, size);
+}
 }
 }
